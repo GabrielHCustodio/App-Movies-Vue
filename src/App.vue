588553f1f0a102440964779b8e792e52671d3a56
Gabriel Henrique <bielriquecustodio@gmail.com>
2022-05-07 23:08:13 -0300

    This is my API data recovery commit.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+
+  </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
+  async mounted() {
+      let KEY_API = '9016a91de1a50285b2a781b9639adbb3'
+      let movies = await fetch(`https://api.themoviedb.org/3/movie/550?api_key=${KEY_API}`, (response) => {
+        return response.json()
+      })
+
+      console.log(movies)
+  }
 }
 </script>
 
