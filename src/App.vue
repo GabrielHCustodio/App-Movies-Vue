f76bc3cba13f481cd0c38ca4cb8389b59cb97ed4
Gabriel Henrique <bielriquecustodio@gmail.com>
2022-05-21 21:46:44 -0300

    This is my commit creating the header of the movie app. Created the search section already retrieving the value reported in the input.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
   <div>
-
+    <header-movie />
   </div>
 </template>
 
 <script>
+import HeaderMovie from "@/components/Header.vue"
 
 export default {
   name: 'App',
-  async mounted() {
+  components: {
+    HeaderMovie
+  }
+}
+</script>
+
+<style>
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+</style>
+
+
+<!--async mounted() {
       let KEY_API = '9016a91de1a50285b2a781b9639adbb3'
       let movies = await fetch(`https://api.themoviedb.org/3/movie/550?api_key=${KEY_API}`, (response) => {
         return response.json()
       })
 
       console.log(movies)
-  }
-}
-</script>
-
-<style>
-</style>
+}-->
---
+++ src/components/Header.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="header">
+        <div class="header-logo">
+            
+        </div>
+
+        <div class="header-input">
+            <input type="text" placeholder="Nome do filme..." v-model="nomeFilme">
+            <img :src="pesq" alt="" @click="pesquisarNome">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderMovie',
+    data() {
+        return {
+            pesq: '/assets/pesquisa.png',
+            nomeFilme: ''
+        }
+    },
+    methods: {
+        pesquisarNome() {
+            console.log(this.nomeFilme)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    background: rgb(0, 110, 255);
+    padding: 10px;
+}
+
+.header-logo {
+    width: 140px;
+    height: 60px;
+    background: #ccc;
+    margin-left: 30px;
+}
+
+.header-input {
+    margin-right: 30px;
+    display: flex;
+    align-items: center;
+}
+
+.header-input input[type=text] {
+    width: 250px;
+    height: 27px;
+    padding: 10px;
+    outline: none;
+}
+
+.header-input img {
+    margin-left: 10px;
+    width: 35px;
+    cursor: pointer;
+}
+</style>
